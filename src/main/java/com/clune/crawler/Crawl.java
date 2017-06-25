7c90c9f219f227c2c61b1e48bb2582340928e015
Andrew Clune <andrewclune@Andrews-MacBook-Pro.local>
2017-06-25 15:33:54 -0600

    Pulling pages.  Rudimentary depth first search in place.  Next up filtering by file type, etc.

--- src/main/java/com/clune/crawler/Crawl.java
+++ src/main/java/com/clune/crawler/Crawl.java
@@ -3,6 +3,18 @@
  */
 package com.clune.crawler;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.io.IOUtils;
+
 /**
  * @author andrewclune
  *
@@ -13,7 +25,37 @@ public class Crawl {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("YAY!");
+		String root = "http://wiprodigital.com";
+		
+		Deque<String> queue = new ArrayDeque<String>();
+		
+		queue.add(root);
+		
+		int i =0;
+		while (!queue.isEmpty() && i++ < 3) {
+			String current = queue.pop();
+			System.out.println("Processing " + current);
+			try {
+				URL url = new URL(current);
+				StringWriter writer = new StringWriter();
+				IOUtils.copy(url.openStream(), writer);
+
+				Pattern pattern = Pattern.compile("href=\"(http://.+?)\"");
+				Matcher match = pattern.matcher(writer.toString());
+
+				while (match.find()) {
+					queue.addLast(match.group(1));
+				}
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
 
 	}
 
