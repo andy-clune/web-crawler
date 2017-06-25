48a01a940c5a1169b3528fe21ab74ee15893a09f
Andrew Clune <andrewclune@Andrews-MacBook-Pro.local>
2017-06-25 11:39:12 -0600

    Made executable JAR.

---
+++ src/main/java/com/clune/crawler/Crawl.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.clune.crawler;
+
+/**
+ * @author andrewclune
+ *
+ */
+public class Crawl {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("YAY!");
+
+	}
+
+}
