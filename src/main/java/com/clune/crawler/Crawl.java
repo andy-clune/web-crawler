88c8d2014958a1081b0a41255c96548e18acdb8f
Andrew Clune <andrewclune@Andrews-MacBook-Pro.local>
2017-07-04 16:55:54 -0600

    Completed basic functionality.

--- src/main/java/com/clune/crawler/Crawl.java
+++ src/main/java/com/clune/crawler/Crawl.java
@@ -3,18 +3,6 @@
  */
 package com.clune.crawler;
 
-import java.io.IOException;
-import java.io.StringWriter;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Queue;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.apache.commons.io.IOUtils;
-
 /**
  * @author andrewclune
  *
@@ -25,12 +13,10 @@ public class Crawl {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String root = "http://wiprodigital.com";
-		
+		String root = args[0];
 		Tree<String> tree = Crawler.crawl(root);
 		SiteMapVisitor visitor = new SiteMapVisitor();
 		Algorithms.depthFirstSearch(tree, visitor);
 		System.out.println(visitor.toString());
 	}
-
 }
--- src/main/java/com/clune/crawler/Crawler.java
+++ src/main/java/com/clune/crawler/Crawler.java
@@ -5,11 +5,11 @@ package com.clune.crawler;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Deque;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import org.jsoup.Jsoup;
@@ -22,58 +22,78 @@ import org.jsoup.select.Elements;
  *
  */
 public class Crawler {
-	
+
 	public static Tree<String> crawl(String url) {
+		String targetDomain = getDomain(url);
+		
 		Deque<Tree<String>> queue = new ArrayDeque<Tree<String>>();
-		Tree<String>  treeRoot = new Tree<String>(url);
+		Tree<String> treeRoot = new Tree<String>(url);
 		queue.add(treeRoot);
-		
-		// Taboo list
+
+		// visited list
 		Set<String> visited = new HashSet<String>();
-		
-		int i =0;
+
+		int i = 0;
 		while (!queue.isEmpty() && i++ < 100) {
 			Tree<String> current = queue.pop();
-			System.out.println("Processing " + current.getValue());
-			
-			try {
-				Document doc = Jsoup.connect(current.getValue()).get();
-				Elements links = doc.select("a[href]");
-				for(Element link : links) {
-					String childUrl = cleanUrl(link.attr("abs:href"));
-					if (!visited.contains(childUrl)) {
-						visited.add(childUrl);
-						Tree<String> childTree = new Tree<String>(childUrl);
-						current.addChild(childTree);
-						queue.addLast(childTree);
+			if (!visited.contains(current.getValue()) && targetDomain.equalsIgnoreCase(getDomain(current.getValue()))) {
+				System.out.println("Processing " + current.getValue());
+				visited.add(current.getValue());
+				try {
+					Document doc = Jsoup.connect(current.getValue()).get();
+					Elements links = doc.select("a[href]");
+					
+					//  This is to dedup links found ON A SINGLE page
+					Set<String> localVisited = new HashSet<String>();
+					for (Element link : links) {
+						String childUrl = cleanUrl(link.attr("abs:href"));
+						if (!localVisited.contains(childUrl)) {
+							localVisited.add(childUrl);
+							Tree<String> childTree = new Tree<String>(childUrl);
+							current.addChild(childTree);
+							queue.addLast(childTree);
+						}
 					}
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
-			} catch (MalformedURLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
 		}
-		
+
 		return treeRoot;
 	}
-	
+
 	private static String cleanUrl(String urlStr) {
 		String cleanUrl = urlStr;
 		int index = urlStr.lastIndexOf("#");
-		if(index != -1) {
+		if (index != -1) {
 			cleanUrl = urlStr.substring(0, index);
 		}
-		
+
 		index = urlStr.lastIndexOf("?");
-		if(index != -1) {
+		if (index != -1) {
 			cleanUrl = cleanUrl.substring(0, index);
 		}
-		
+
 		return cleanUrl;
+
+	}
+	
+	private static String getDomain(String url) {
+		String domain = "";
+		try {
+			URI uri = new URI(url);
+			domain = uri.getHost();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+		return domain;
 	}
 
 }
